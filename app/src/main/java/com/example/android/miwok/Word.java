e71fad8b095156a7d6adb1200019f7f8f759b6e2
thehme <bcortez23@gmail.com>
2018-07-02 18:50:29 -0400

    wip

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -6,12 +6,17 @@ public class Word {
 
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageResourceId;
 
     public  Word(String miwokTranslation, String defaultTranslation) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
     }
-
+    public Word(String miwokTranslation, String defaultTranslation, int imageResourceId) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mImageResourceId = imageResourceId;
+    }
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
