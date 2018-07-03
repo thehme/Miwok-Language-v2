cb1305d0655aaf62d5e940f13d4eefbe79a579d3
thehme <bcortez23@gmail.com>
2018-07-03 00:01:24 -0400

    added new constructor and use of it in numbers activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -18,16 +18,16 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         ArrayList<Word> numbers = new ArrayList<Word>();
-        numbers.add(new Word("one", "lutti"));
-        numbers.add(new Word("two", "ottiko"));
-        numbers.add(new Word("three", "tolookosu"));
-        numbers.add(new Word("four", "oyyisa"));
-        numbers.add(new Word("five", "massokka"));
-        numbers.add(new Word("six", "temmokka"));
-        numbers.add(new Word("seven", "kenekaku"));
-        numbers.add(new Word("eight", "kawinta"));
-        numbers.add(new Word("nine", "wo'e"));
-        numbers.add(new Word("ten", "na'aacha"));
+        numbers.add(new Word("one", "lutti", R.drawable.number_one));
+        numbers.add(new Word("two", "ottiko", R.drawable.number_two));
+        numbers.add(new Word("three", "tolookosu", R.drawable.number_three));
+        numbers.add(new Word("four", "oyyisa", R.drawable.number_four));
+        numbers.add(new Word("five", "massokka", R.drawable.number_five));
+        numbers.add(new Word("six", "temmokka", R.drawable.number_six));
+        numbers.add(new Word("seven", "kenekaku", R.drawable.number_seven));
+        numbers.add(new Word("eight", "kawinta", R.drawable.number_eight));
+        numbers.add(new Word("nine", "wo'e", R.drawable.number_nine));
+        numbers.add(new Word("ten", "na'aacha", R.drawable.number_ten));
 
         WordAdapter itemsAdapter = new WordAdapter(this, numbers);
 
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -24,4 +24,6 @@ public class Word {
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
+
+    public int getImageResourceId() { return mImageResourceId; }
 }
